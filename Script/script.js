0c4b15a4df23bc640ec616f9a632dde486212c4d
AlessioD <106388894+AlessioD200@users.noreply.github.com>
2024-10-30 15:20:16 +0100

    Update

---
+++ Script/script.js
@@ -0,0 +1,29 @@
+let slideIndex = 0;
+autoShowSlides();
+showSlides(slideIndex);
+
+function changeSlide(n) {
+    showSlides(slideIndex += n);
+}
+function autoShowSlides() {
+    const slides = document.getElementsByClassName("slide");
+    for (let slide of slides) {
+        slide.style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1; }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(autoShowSlides, 3000); // Change image every 3 seconds
+}
+
+function showSlides(n) {
+    const slides = document.getElementsByClassName("slide");
+    if (n >= slides.length) { slideIndex = 0; }
+    if (n < 0) { slideIndex = slides.length - 1; }
+
+    for (let slide of slides) {
+        slide.style.display = "none";
+    }
+
+    slides[slideIndex].style.display = "block";
+}
